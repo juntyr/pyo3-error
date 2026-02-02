172e8352a2b8cbd0537dee40eb05e276cb4d2d62
juntyr <juntyr@users.noreply.github.com>
2026-02-02 08:36:28 +0000

    deploy: bc6981d0029f139bc3bc56fb43d7cbb1ec02e165

--- trait.impl/core/convert/trait.From.js
+++ trait.impl/core/convert/trait.From.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["pyo3_error",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://docs.rs/pyo3/0.27/pyo3/err/struct.PyErr.html\" title=\"struct pyo3::err::PyErr\">PyErr</a>&gt; for <a class=\"struct\" href=\"pyo3_error/struct.PyErrChain.html\" title=\"struct pyo3_error::PyErrChain\">PyErrChain</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"pyo3_error/struct.PyErrChain.html\" title=\"struct pyo3_error::PyErrChain\">PyErrChain</a>&gt; for <a class=\"struct\" href=\"https://docs.rs/pyo3/0.27/pyo3/err/struct.PyErr.html\" title=\"struct pyo3::err::PyErr\">PyErr</a>"]]]]);
+    const implementors = Object.fromEntries([["pyo3_error",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://docs.rs/pyo3/0.27/pyo3/err/struct.PyErr.html\" title=\"struct pyo3::err::PyErr\">PyErr</a>&gt; for <a class=\"struct\" href=\"pyo3_error/struct.PyErrChain.html\" title=\"struct pyo3_error::PyErrChain\">PyErrChain</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"pyo3_error/struct.PyErrChain.html\" title=\"struct pyo3_error::PyErrChain\">PyErrChain</a>&gt; for <a class=\"struct\" href=\"https://docs.rs/pyo3/0.27/pyo3/err/struct.PyErr.html\" title=\"struct pyo3::err::PyErr\">PyErr</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[816]}
+//{"start":59,"fragment_lengths":[820]}
--- trait.impl/core/marker/trait.UnsafeUnpin.js
+++ trait.impl/core/marker/trait.UnsafeUnpin.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["pyo3_error",[["impl UnsafeUnpin for <a class=\"struct\" href=\"pyo3_error/struct.DowncastToPyErr.html\" title=\"struct pyo3_error::DowncastToPyErr\">DowncastToPyErr</a>",1,["pyo3_error::DowncastToPyErr"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"pyo3_error/struct.ErrorNoPyErr.html\" title=\"struct pyo3_error::ErrorNoPyErr\">ErrorNoPyErr</a>",1,["pyo3_error::ErrorNoPyErr"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"pyo3_error/struct.IoErrorToPyErr.html\" title=\"struct pyo3_error::IoErrorToPyErr\">IoErrorToPyErr</a>",1,["pyo3_error::IoErrorToPyErr"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"pyo3_error/struct.PyErrChain.html\" title=\"struct pyo3_error::PyErrChain\">PyErrChain</a>",1,["pyo3_error::PyErrChain"]]]]]);
+    const implementors = Object.fromEntries([["pyo3_error",[["impl UnsafeUnpin for <a class=\"struct\" href=\"pyo3_error/struct.DowncastToPyErr.html\" title=\"struct pyo3_error::DowncastToPyErr\">DowncastToPyErr</a>",0,1,["pyo3_error::DowncastToPyErr"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"pyo3_error/struct.ErrorNoPyErr.html\" title=\"struct pyo3_error::ErrorNoPyErr\">ErrorNoPyErr</a>",0,1,["pyo3_error::ErrorNoPyErr"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"pyo3_error/struct.IoErrorToPyErr.html\" title=\"struct pyo3_error::IoErrorToPyErr\">IoErrorToPyErr</a>",0,1,["pyo3_error::IoErrorToPyErr"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"pyo3_error/struct.PyErrChain.html\" title=\"struct pyo3_error::PyErrChain\">PyErrChain</a>",0,1,["pyo3_error::PyErrChain"]]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[748]}
+//{"start":59,"fragment_lengths":[756]}
